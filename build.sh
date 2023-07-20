55734b6699a8d411081e1ab4210db0e3c7084c99
Christophe Fergeau <cfergeau@redhat.com>
2023-07-20 13:02:53 +0300

    Build universal binary with makefat

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 #
 GOARCH=amd64 GOOS=darwin go build -o printarch.amd64 .
 GOARCH=arm64 GOOS=darwin go build -o printarch.arm64 .
-lipo -create -output printarch printarch.amd64 printarch.arm64
+lipo -create -output printarch.lipo printarch.amd64 printarch.arm64
+
+# now try with makefat instead of lipo
+go get github.com/randall77/makefat
+GOBIN=$(pwd) go install github.com/randall77/makefat
+./makefat printarch.makefat printarch.amd64 printarch.arm64
